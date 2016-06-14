c285b5f9f57c510a55d72a322fc1e37f367f918b
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2016-06-14 14:17:31 +0300

    DENSWXCDIG-12 / Implemented drupal menu sync

--- ckanext/drupal_menu_sync/plugin.py
+++ ckanext/drupal_menu_sync/plugin.py
@@ -5,42 +5,57 @@ from pylons import config
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from pylons.decorators.cache import beaker_cache
+import logging
+import json
+import re
+from ckan.common import _, g
 
-sections = dict(
-  main='main-menu',
-  footer='menu-footer')
+log = logging.getLogger(__name__)
 
+def menu_links(section=None):
+    # Drupal URL is the root of the domain without the slash
+    drupal_url = config.get('drupal.site_url')
+    # fallback links
+    json_str = '[{"title":"Home","link":"'+drupal_url+'"},' \
+               '{"title":"Datasets","link":"'+drupal_url+'/data/dataset"},' \
+               '{"title":"Organisations","link":"'+drupal_url+'/data/organization"},' \
+               '{"title":"Groups","link":"'+drupal_url+'/data/group"},' \
+               '{"title":"Strategies","link":"'+drupal_url+'/strategies"},' \
+               '{"title":"Toolkit","link":"'+drupal_url+'/toolkit"},' \
+               '{"title":"Apps & Ideas","link":"'+drupal_url+'/data/showcase"}]'
+    links = json.loads(json_str)
+    # request links from Drupal
+    r = None
+    section_menu = []
+    try:
+        r = requests.get(drupal_url + '/menu_export', verify=False, timeout=10)
+    except requests.exceptions.Timeout:
+        log.warning(drupal_url + '/menu_export connection timeout')
+    except requests.exceptions.TooManyRedirects:
+        log.warning(drupal_url + '/menu_export too many redirects')
+    except requests.exceptions.RequestException as e:
+        log.error(e.message)
 
-@beaker_cache(expire=3600)
-def drupal_menu_get(section=None):
-  drupal_url = config.get('drupal.site_url') or \
-    routes.url_for('/', qualified=True).replace(':5000', '')
-  menus_url = urlparse.urljoin(drupal_url, 'ckanext/menus/sync')
-
-  try:
-      menus = requests.get(menus_url).json()
-  except Exception:
-      menus = {}
-
-  if section:
-    return menus.get(sections[section])
-  return menus
-
+    if r:
+        links = r.json()
+    if section:
+      for item in links[section]:
+        section_menu.append(item)
+      return section_menu  
+    return links
 
 class Drupal_Menu_SyncPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
-
+    
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'drupal_menu_sync')
 
     # ITemplateHelpers
-
     def get_helpers(self):
       return {
-        'drupal_menu_get': drupal_menu_get,
+        'menu_links': menu_links
         }
